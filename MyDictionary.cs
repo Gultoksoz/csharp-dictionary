6c6e1cf82455479c4bb2ca76c224041ea0376794
Gul <62958847+Gultoksoz@users.noreply.github.com>
2021-02-18 21:43:47 +0300

    Add files via upload

---
+++ MyDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<Tkey,Tvalue>
+    {
+        Tkey[] keys;
+        Tkey[] tempkeys;
+        Tvalue[] values;
+        Tvalue[] tempvalues;
+
+        public Tkey[] Keys { get { return keys; } }
+        public Tvalue[] Values { get { return values; } }
+
+        public MyDictionary()
+        {
+            keys = new Tkey[0];
+            values = new Tvalue[0];
+        }
+
+        public void Add(Tkey tkey, Tvalue tvalue)
+        {
+            tempkeys = keys;
+            tempvalues = values;
+
+            keys = new Tkey[keys.Length + 1];
+            values = new Tvalue[values.Length + 1];
+
+            for (int i = 0; i < tempkeys.Length; i++)
+            {
+                keys[i] = tempkeys[i];
+            }
+
+            for (int i = 0; i < tempvalues.Length; i++)
+            {
+                values[i] = tempvalues[i];
+            }
+
+            keys[keys.Length - 1] = tkey;
+            values[values.Length - 1] = tvalue;
+        }
+
+        public void List()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("No..: " + Keys[i] + " Name..: " + Values[i]);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Dictionary;
+using System;
+
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> studentList = new MyDictionary<int, string>();
+
+            studentList.Add(1 , "Yaren Alasan");
+            studentList.Add(2, "Kunt Korali");
+            studentList.Add(3,"Kartal Sonmez");
+            studentList.Add(4, "Yasemin Karaduman");
+
+            studentList.List();
+        }
+    }
+}
